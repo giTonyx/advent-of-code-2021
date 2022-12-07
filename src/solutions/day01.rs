a928eb0ae0639aedcd63474ba9e172353ee267d5
Antonio Scotti <a.scotti@criteo.com>
2022-12-07 14:12:42 +0100

    Adding solution for day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,20 +1,36 @@
-use crate::solver::Solver;
+use crate::solver::{ReadExt, Solver};
 use std::io::Read;
 
 pub struct Problem;
 
 impl Solver for Problem {
-    type Input = ();
-    type Output1 = u64;
-    type Output2 = u64;
+    type Input = Vec<u32>;
+    type Output1 = usize;
+    type Output2 = usize;
 
-    fn parse_input<R: Read>(&self, r: R) -> Self::Input {}
+    fn parse_input<R: Read>(&self, r: R) -> Self::Input {
+        r.split_lines()
+    }
 
     fn solve_first(&self, input: &Self::Input) -> Self::Output1 {
-        0
+        let mut total:usize = 0;
+        for idx in 1..input.len(){
+            if input[idx] > input[idx-1] {
+                total += 1;
+            }
+        }
+        total
     }
 
     fn solve_second(&self, input: &Self::Input) -> Self::Output2 {
-        0
+        let mut total:usize = 0;
+        for idx in 3..input.len(){
+            let first_window = input[idx-3] + input[idx-2] + input[idx-1];
+            let second_window = input[idx-2] + input[idx-1] + input[idx];
+            if second_window > first_window {
+                total += 1;
+            }
+        }
+        total
     }
 }
